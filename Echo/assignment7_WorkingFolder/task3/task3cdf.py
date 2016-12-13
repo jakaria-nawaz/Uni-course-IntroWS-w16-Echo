44a85472f2367b4b0482a4e4fcb378cfe712fabd
Md Jakaria Nawaz <Md Jakaria Nawaz>
2016-12-13 22:35:50 +0100

    Number3 calculation complete

--- Echo/assignment7_WorkingFolder/task3/task3cdf.py
+++ Echo/assignment7_WorkingFolder/task3/task3cdf.py
@@ -31,9 +31,11 @@ def drawCDF(sumDiceResult,n,rn):
     print("Median val using np: ", theMedian)
     print("Median val manually: ", sorted1[50])
     totalNum9 = sum(i <= 9 for i in sorted1)
-    totalNum9per = ((totalNum9*100)/100)/100
+    totalNum9per = ((totalNum9*100)/n)/100
     print("Probability of dice sum to be equal or less than 9: ",totalNum9per)
+
     yvals = np.arange(len(sorted1))/float(len(sorted1))
+
     plt.plot(sorted1, yvals)
     plt.plot((0, 12), (.5, .5))
     plt.plot((9, 9), (0, 1))
@@ -50,6 +52,7 @@ def drawCDF(sumDiceResult,n,rn):
     plt.xlabel('Sum Results (x)')
     plt.grid('on')
 
+    print('\n')
     #plt.switch_backend('TkAgg')
     mng = plt.get_current_fig_manager()
     mng.window.state('zoomed')
@@ -70,6 +73,20 @@ def drawCDF(sumDiceResult,n,rn):
 #     plt.xlabel('Sum Results')
 #     plt.show()
 
+def pointDistance(listSumOf1, listSumOf2, n):
+    cumsumS1=np.cumsum(listSumOf1)
+    #print(cumsumMain[0])
+    #print(type(cumsumMain))
+    normedcumsumS1=[x/float(cumsumS1[-1]) for x in cumsumS1]
+
+    cumsumS2=np.cumsum(listSumOf2)
+    #print(cumsumMain[0])
+    #print(type(cumsumMain))
+    normedcumsumS2=[x/float(cumsumS2[-1]) for x in cumsumS2]
+
+    D = [a - b for a, b in zip(normedcumsumS1, normedcumsumS2)]
+    print('Maximum point distance of CDFs generated from simulation 1 and 2 for ',n,' data: ', max(D))
+
 def main():
     global dice1Result
     dice1Result = []
@@ -80,7 +97,8 @@ def main():
     global sumDiceResult2
     sumDiceResult2 = []
 
-    #Simulation 1
+
+    #Simulation 1 for 100
     for x in range(0, 100):
         dice_roll()
 
@@ -89,30 +107,74 @@ def main():
     # print(dice2Result)
     # print(sumDiceResult)
 
-    print('Results for simulation 1')
+    print('Results for simulation 1 for 100 samples')
     #Draw histogram
     drawHistogram(sumDiceResult,100,1)
     #Draw CDF
     drawCDF(sumDiceResult,100,1)
     #drawCDF2(sumDiceResult)
 
-    #Simulation 2
-    global dice1Result
+    #Simulation 2 for 100
     dice1Result = []
-    global dice2Result
     dice2Result = []
+
 
     for x in range(0, 100):
         dice_roll()
 
     sumDiceResult2 = [x + y for x, y in zip(dice1Result, dice2Result)]
 
-    print('Results for simulation 2')
+    print('\nResults for simulation 2 for 100 samples')
     #Draw histogram
     drawHistogram(sumDiceResult2,100,2)
     #Draw CDF
     drawCDF(sumDiceResult2,100,2)
     #drawCDF2(sumDiceResult)
+
+    pointDistance(sumDiceResult, sumDiceResult2,100)
+
+    #Simulation 1 for 1000 #####################################################
+    sumDiceResult = []
+    sumDiceResult2 = []
+    dice1Result = []
+    dice2Result = []
+
+    #Simulation 1 for 1000
+    for x in range(0, 1000):
+        dice_roll()
+
+    sumDiceResult = [x + y for x, y in zip(dice1Result, dice2Result)]
+    # print(dice1Result)
+    # print(dice2Result)
+    # print(sumDiceResult)
+
+    print('Results for simulation 1 for 1000 samples')
+    #Draw histogram
+    drawHistogram(sumDiceResult,1000,1)
+    #Draw CDF
+    drawCDF(sumDiceResult,1000,1)
+    #drawCDF2(sumDiceResult)
+
+    #Simulation 2 for 1000
+    dice1Result = []
+    dice2Result = []
+
+
+    for x in range(0, 1000):
+        dice_roll()
+
+    sumDiceResult2 = [x + y for x, y in zip(dice1Result, dice2Result)]
+
+    print('\nResults for simulation 2 for 1000 samples')
+    #Draw histogram
+    drawHistogram(sumDiceResult2,1000,2)
+    #Draw CDF
+    drawCDF(sumDiceResult2,1000,2)
+    #drawCDF2(sumDiceResult)
+
+    pointDistance(sumDiceResult, sumDiceResult2,1000)
+
+
 
 if __name__ == '__main__':
     main()
